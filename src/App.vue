f09e74ba8497ba810a3e29f68a1611e5eb06b15c
jamesm2w <11365435+jamesm2w@users.noreply.github.com>
2021-10-15 18:09:00 +0100

    Added Vue code.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Slideshow from './components/Slideshow.vue';
+</script>
+
+<template>
+  <img class="rounded-full shadow-md mx-auto hover:shadow-2xl transition-all duration-300 z-10 relative" width="350" alt="jamesm2w Profile Image" src="./assets/profileimage.png" />
+  
+  <Slideshow class="z-0" />
+  
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Slideshow.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="max-w-full absolute m-auto top-0 left-0 w-screen h-screen">
+        <transition name="fade">
+            <div v-show="index == 0" class="transition-all duration-700">
+                <img class="w-full" src="./../assets/SlideshowBeach.jpg" alt="Picture of a Beach">
+                <div class="text">Image Credit: James Macer-Wright</div>
+            </div>
+        </transition>
+        <transition name="fade">
+            <div v-show="index == 1" class="transition-all duration-700">
+                <img class="w-full" src="./../assets/SlideshowClouds.jpg" alt="Picture of clouds">
+                <div class="text">Image Credit: James Macer-Wright</div>
+            </div>
+        </transition>
+        <transition name="fade">
+            <div v-show="index == 2" class="transition-all duration-700">
+                <img class="w-full" src="./../assets/SlideshowRocks.jpg" alt="Picture of some rocks">
+                <div class="text">Image Credit: James Macer-Wright</div>
+            </div>
+        </transition>
+        <transition name="fade">
+            <div v-show="index == 3" class="transition-all duration-700">
+                <img class="w-full" src="./../assets/SlideshowSea.jpg" alt="Picture of the Sea">
+                <div class="text">Image Credit: James Macer-Wright</div>
+            </div>
+        </transition>
+    </div>    
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+
+let index = ref(0);
+
+function incrementIndex () {
+    index.value++;
+    index.value = index.value % 4;
+}
+setInterval(incrementIndex, 5000);
+</script>
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: all 1s ease-in-out;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+  transition: all 1s ease-in-out;
+}
+
+.fade-enter-to, .fade-leave {
+    opacity: 1;
+    transition: all 1s ease-in-out;
+}
+</style>

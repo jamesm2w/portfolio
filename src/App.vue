611565db1b5e1d61c27e95ace444c918080b8cc1
jamesm2w <11365435+jamesm2w@users.noreply.github.com>
2021-10-19 22:20:42 +0100

    Add Slideshow Background

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,29 @@ import Slideshow from './components/Slideshow.vue';
 </script>
 
 <template>
-  <img class="rounded-full shadow-md mx-auto hover:shadow-2xl transition-all duration-300 z-10 relative" width="350" alt="jamesm2w Profile Image" src="./assets/profileimage.png" />
+  <img class="rounded-full shadow-md mx-auto hover:shadow-2xl transition-all duration-300 z-10 relative" width="250" alt="jamesm2w Profile Image" src="./assets/profileimage.png" />
   
   <Slideshow class="z-0" />
   
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="sticky py-2 text-5xl font-thin w-full block text-white">jamesm2w</div>
+
+  <div class="sticky py-4 text-xl font-medium block">
+    Computer Scientist
+  </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #DCDFE4;
   margin-top: 60px;
 }
+
+html {
+  background-color: #282C34;
+}
 </style>
+
--- src/components/Slideshow.vue
+++ src/components/Slideshow.vue
@@ -1,29 +1,37 @@
 <template>
-    <div class="max-w-full absolute m-auto top-0 left-0 w-screen h-screen">
-        <transition name="fade">
-            <div v-show="index == 0" class="transition-all duration-700">
-                <img class="w-full" src="./../assets/SlideshowBeach.jpg" alt="Picture of a Beach">
-                <div class="text">Image Credit: James Macer-Wright</div>
-            </div>
+    <div class="max-w-full absolute m-auto top-0 left-0 w-screen h-screen slideshow">
+
+        <transition name="fade" mode="out-in">
+            <img 
+                v-if="index == 0"
+                class="w-full h-full filter blur-sm" 
+                src="./../assets/SlideshowBeach.jpg" 
+                alt="Picture of a Beach"
+            >
+
+            <img
+                v-else-if="index == 1"
+                class="w-full h-full filter blur-sm" 
+                src="./../assets/SlideshowClouds.jpg" 
+                alt="Picture of clouds"
+            >
+
+            <img
+                v-else-if="index == 2"
+                class="w-full h-full filter blur-sm" 
+                src="./../assets/SlideshowRocks.jpg" 
+                alt="Picture of some rocks"
+            >
+
+            <img
+                v-else-if="index == 3"
+                class="w-full h-full filter blur-sm" 
+                src="./../assets/SlideshowSea.jpg" 
+                alt="Picture of the Sea"
+            >
         </transition>
-        <transition name="fade">
-            <div v-show="index == 1" class="transition-all duration-700">
-                <img class="w-full" src="./../assets/SlideshowClouds.jpg" alt="Picture of clouds">
-                <div class="text">Image Credit: James Macer-Wright</div>
-            </div>
-        </transition>
-        <transition name="fade">
-            <div v-show="index == 2" class="transition-all duration-700">
-                <img class="w-full" src="./../assets/SlideshowRocks.jpg" alt="Picture of some rocks">
-                <div class="text">Image Credit: James Macer-Wright</div>
-            </div>
-        </transition>
-        <transition name="fade">
-            <div v-show="index == 3" class="transition-all duration-700">
-                <img class="w-full" src="./../assets/SlideshowSea.jpg" alt="Picture of the Sea">
-                <div class="text">Image Credit: James Macer-Wright</div>
-            </div>
-        </transition>
+        
+
     </div>    
 </template>
 
@@ -39,16 +47,17 @@ function incrementIndex () {
 setInterval(incrementIndex, 5000);
 </script>
 <style scoped>
+
 .fade-enter-active, .fade-leave-active {
-  transition: all 1s ease-in-out;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-  transition: all 1s ease-in-out;
+    transition: opacity 1s ease;
 }
 
-.fade-enter-to, .fade-leave {
-    opacity: 1;
-    transition: all 1s ease-in-out;
+.fade-enter-from, .fade-leave-to {
+    opacity: 0;
 }
+
+.slideshow img {
+    object-fit: cover;
+}
+
 </style>

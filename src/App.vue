24e3d7af35948b335972a17cc86f5b978d1113c5
jamesm2w <11365435+jamesm2w@users.noreply.github.com>
2021-10-20 17:57:10 +0100

    Fixed year display. Added some explanation text.

--- src/App.vue
+++ src/App.vue
@@ -23,15 +23,20 @@
       jamesm2w
     </div>
 
-    <div class="sticky py-4 text-xl font-medium block">Computer Scientist from Dorset, UK</div>
+    <div class="sticky py-4 text-xl font-medium block">
+      Computer Scientist from Dorset, UK
+    </div>
 
-    <Button @click="
+    <Button
+      @click="
         $refs.projectSection.scrollIntoView({
           block: 'start',
           inline: 'nearest',
           behavior: 'smooth',
         })
-      ">View Projects</Button>
+      "
+      >View Projects</Button
+    >
   </div>
 
   <div ref="projectSection" class="min-h-screen w-screen">
@@ -77,24 +82,39 @@
     >
 
     <div class="font-mono font-semibold text-5xl">Technologies</div>
+    <p class="text-center m-auto sm:w-full md:w-9/12">
+      All the programming technologies I have some experience working with.
+      However, I'm always up for a new challenge and to learn new languages as
+      they're required by the project I'm working on.
+    </p>
     <div class="flex flex-row flex-wrap w-9/12 justify-center m-auto">
       <Pill v-for="language in projectData.languages" :key="language">
         {{ language }}
       </Pill>
     </div>
 
-    <hr class="w-9/12 my-2 m-auto">
+    <hr class="w-9/12 my-2 m-auto" />
 
     <div class="font-mono font-semibold text-5xl" ref="sectionProjects">
       Projects
     </div>
-    <Button class="text-sm" @click="
+    <p class="text-center m-auto sm:w-full md:w-9/12">
+      Personal Projects which have been of finished enough to publish on the
+      internet. Most are in various states of completion, if you want me to work
+      on something just contact me and I'll see if I can do it. Alternatively,
+      create a pull request :)
+    </p>
+    <Button
+      class="text-sm"
+      @click="
         $refs.projectSection.scrollIntoView({
           block: 'start',
           inline: 'nearest',
           behavior: 'smooth',
         })
-      ">Top</Button>
+      "
+      >Click to go back up</Button
+    >
     <div class="w-10/12 hero m-auto pb-10">
       <Project
         v-for="project in projectData.projects"
@@ -103,18 +123,27 @@
       ></Project>
     </div>
 
-    <hr class="w-9/12 my-2 m-auto">
+    <hr class="w-9/12 my-2 m-auto" />
 
     <div class="font-mono font-semibold text-5xl" ref="sectionCoursework">
       Coursework
     </div>
-    <Button class="text-sm" @click="
+    <p class="text-center m-auto sm:w-full md:w-9/12">
+      Coursework/Graded Projects from school and university, which I have
+      completed and wanted to make a note of. In most cases the source code can
+      not be published as the task could be reused.
+    </p>
+    <Button
+      class="text-sm"
+      @click="
         $refs.projectSection.scrollIntoView({
           block: 'start',
           inline: 'nearest',
           behavior: 'smooth',
         })
-      ">Top</Button>
+      "
+      >Click to go back up</Button
+    >
     <div class="w-10/12 hero m-auto pb-10">
       <Project
         v-for="project in projectData.coursework"
@@ -123,20 +152,24 @@
       ></Project>
     </div>
 
-    <hr class="w-9/12 my-2 m-auto">
+    <hr class="w-9/12 my-2 m-auto" />
 
     <div class="font-mono font-semibold text-4xl" ref="socialLinks">
       Contact & Links
     </div>
-
-    <Button class="text-sm" @click="
+    <p class="text-center m-auto sm:w-full md:w-9/12">Contact Me!</p>
+    <Button
+      class="text-sm"
+      @click="
         $refs.projectSection.scrollIntoView({
           block: 'start',
           inline: 'nearest',
           behavior: 'smooth',
         })
-      ">Top</Button>
-    
+      "
+      >Click to go back up</Button
+    >
+
     <div class="flex flex-row flex-wrap w-9/12 justify-center m-auto">
       <Pill v-for="linkObj in projectData.socialLinks" :key="linkObj.name">
         <a :href="linkObj.url" target="_blank">{{ linkObj.name }}</a>
@@ -144,8 +177,14 @@
     </div>
   </div>
   <div class="bg-gray-900 h-10 py-2 mt-2">
-    <span class="float-left font-mono ml-2">Copyright (C) James Macer-Wright {{ (new Date()).getYear() }}</span>
-    <span class="float-right font-mono mr-2 underline"><a target="_blank" href="https://github.com/jamesm2w/jamesm2w.github.io">Source (Github)</a></span>
+    <span class="float-left font-mono ml-2"
+      >Copyright (C) James Macer-Wright {{ new Date().getFullYear() }}</span
+    >
+    <span class="float-right font-mono mr-2 underline"
+      ><a target="_blank" href="https://github.com/jamesm2w/jamesm2w.github.io"
+        >Source (Github)</a
+      ></span
+    >
   </div>
 </template>
 <script setup>
@@ -178,7 +217,7 @@ html {
   display: grid;
   align-items: flex-start;
   grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
-  grid-gap: .5rem;
+  grid-gap: 0.5rem;
 }
 </style>
 

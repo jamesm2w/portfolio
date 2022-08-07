2579ca5d8b2de3dcbbf7476dde8a0d9e7679d6f9
James <11365435+jamesm2w@users.noreply.github.com>
2022-08-07 10:36:06 +0100

    Bump images to latest iteration.

--- src/components/Slideshow.vue
+++ src/components/Slideshow.vue
@@ -2,18 +2,16 @@
     <div class="max-w-full absolute m-auto top-0 left-0 w-screen h-screen slideshow">
 
         <transition name="fade" mode="out-in">
-            <img 
+            <!-- <img 
                 class="w-full h-full filter blur-sm" 
                 src="./../assets/SlideshowDorsetHill.jpg" 
                 alt="Picture of a Hill in Dorset"
-            >
-            <!--
+            > -->
             <img
-                v-else-if="index == 1"
                 class="w-full h-full filter blur-sm" 
                 src="./../assets/SlideshowClouds.jpg" 
                 alt="Picture of clouds"
-            >
+            > <!--
 
             <img
                 v-else-if="index == 2"
